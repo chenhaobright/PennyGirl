7349d8889eeff0e415c4ed25497a3f7fa610e502
chenhaobright <chenhaobright@gmail.com>
2014-07-25 00:25:10 +0800

    add  sound

---
+++ Classes/Define.h
@@ -0,0 +1,10 @@
+#ifndef __DEFINE_H__
+#define __DEFINE_H__
+
+
+#define PISTOL1_MP3			("sound/pistol1.mp3")
+#define PISTOL2_MP3			("sound/pistol2.mp3")
+#define ROLL_MP3			("sound/roll.mp3")
+#define SHOT_EMPTY_MP3		("sound/shot_empty.mp3")
+
+#endif	//__DEFINE_H__
